c79ab243729bed3f1ef6c945e929c70f01cc757e
sdlxnvv <murodsadulloxonov@yandex.ru>
2025-03-23 17:52:54 +0500

    uchinchi

--- src/FutureControlExample.java
+++ src/FutureControlExample.java
@@ -24,6 +24,7 @@ public class FutureControlExample {
         if (responseUzCard.get()) {
             System.out.println("UzCard da pul bor!");
             System.out.println("Murod hozirgi ");
+            System.out.println("Murod hozirgi ");
         }
         if (responseHumoCard.get()) {
             System.out.println("HumoCard da pul kop");
